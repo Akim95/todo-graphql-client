d145aa4b8ff05291df17e992a7bddccf97a34de3
akim95 <m.akmalhakim95@gmail.com>
2016-08-20 16:53:07 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+import React, { Component } from 'react';
+// import graphql-tag and react-apollo here
+
+import _ from 'lodash';
+
+// App class
+class App extends Component {
+  // constructor
+
+
+  // handleCreateTodo() method here
+
+  // render
+}
+
+// query here
+
+
+// mutation here
+
+
+// query
+
+
+// mutation
+
+
+// export component
+export default TodoWithDataAndMutations;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// import apollo-client and react-apollo
+
+// create apollo connection
+
+ReactDOM.render(
+  // ApolloProvider here
+  ,
+  document.getElementById('root')
+);
